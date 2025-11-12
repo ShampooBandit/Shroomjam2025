d6e651d02ccb4b8cf61fbfad339b84bd4f3a427e
Sparkwave <Plazmoid3@gmail.com>
2025-11-12 22:49:27 +0100

    Gallery

--- Scenes/gallery_video_list.gd
+++ Scenes/gallery_video_list.gd
@@ -1,6 +1,13 @@
 extends GridContainer
 
+@export var container_scene: PackedScene
+@export var video_player: GalleryPlayer
+
 func _ready():
 	var vids = DirAccess.open("res://Videos/")
 	for file in vids.get_files():
-		print(file)
+		if file.ends_with(".ogv"):
+			var instance: GalleryVideoContainer = container_scene.instantiate()
+			instance.video_name = file.replace(".ogv", "")
+			instance.video_player = video_player
+			add_child(instance)
